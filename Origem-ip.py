5c5ab8da9761b67bf4e9d6eb44d29283b0d63826
pedroalpacheco <pedro.pacheco.a@gmail.com>
2019-04-17 09:48:58 -0300

    Pegando pais

--- Origem-ip.py
+++ Origem-ip.py
@@ -1,5 +1,23 @@
-import os
+from ipwhois import IPWhois
+from pprint import pprint
 
-#cmdwhois = 'whois'
+obj = IPWhois('221.2.44.75')
 
-os.system('whois ' + '221.2.44.75')
+#Pega completo
+#results = obj.lookup_rdap(depth=1)
+
+#Pega ip
+#results2 = obj.address
+
+#Whois completo
+#result3 = obj.lookup_whois()
+
+result4 = obj.lookup_whois()
+
+#Pega pais
+pprint(result4["nets"][0]['country'])
+
+
+
+
+
